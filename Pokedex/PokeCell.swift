e766fd200dbf8c5bd5d65fc25f5c7a69304cf85f
rv2011 <raghavvashisht@Yavnikas-MacBook-Air.local>
2017-04-11 22:50:01 +0530

    Added various cool Functionality like parsing through CSV file, implementing data from CSV to UI elements, and added SearchBar Functionality

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Raghav Vashisht on 11/04/17.
+//  Copyright © 2017 Raghav Vashisht. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Raghav Vashisht on 10/04/17.
+//  Copyright © 2017 Raghav Vashisht. All rights reserved.
+//
+
+import Foundation
